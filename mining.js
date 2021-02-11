bc796f3712883a0b48ca1a6d0e6b4af62787c31c
Joann Feng <joannfeng@wireless-10-148-218-124.public.utexas.edu>
2021-02-11 11:29:46 -0600

    started planning, created html, css, js files

--- mining.js
+++ mining.js
@@ -1,1 +1,19 @@
+/**
+ * TODO:
+ * layers of dirt GOOD
+ * There are three depths of wall: 
+ *  the tan soil, which can be broken through in one hit, 
+ *  the darker soil, which needs two hits to be broken through, 
+ *  and the large rocks, which each need three hits to be broken through.
+ * pick tool (targets 1 square, lose 1 life)
+ * hammer tool (targets 9 squares, lose ??? lives)
+ * upon click, layer disappears
+ * lives
+ * "gems" / blocks to find
+ * 
+ * sounds effects?
+ */
 
+function removeElem(elem) {
+    document.getElementById(elem).style.visibility='hidden';
+}
